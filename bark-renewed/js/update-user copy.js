c1733ff41366485b69bd882e9a251d335c75730c
Z-hyeon <jihyeon2368@naver.com>
2024-04-10 20:31:03 +0900

    feat update-user

--- bark-renewed/js/update-user copy.js
+++ bark-renewed/js/update-user copy.js
@@ -6,17 +6,16 @@ function openPop() {
 function closePop() {
   document.getElementById("popup_layer").style.display = "none";
 }
-
-
-
 window.onload = function () {
+
+
 const hypenTel = (target) => {
   target.value = target.value
     .replace(/[^0-9]/g, "")
     .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
 };
 
-const pwdBtn = document.querySelector(".pwdBtn");
+const pwdBtn = document.querySelector(".pwd-btn");
 pwdBtn.onclick =  function () {
     document.querySelector(".menu").classList.toggle("expand");
   };
