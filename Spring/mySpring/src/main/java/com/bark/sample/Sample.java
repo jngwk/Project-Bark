acc863201cfd89d2ff517169fdbafdb3cb0eaaa4
jngwk <jngwk.ch@gmail.com>
2024-04-14 21:16:22 +0900

    commit after reset

---
+++ Spring/mySpring/src/main/java/com/bark/sample/Sample.java
@@ -0,0 +1,14 @@
+package com.bark.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sample {
+private Integer mno;
+private String firstName;
+private String lastName;
+}
