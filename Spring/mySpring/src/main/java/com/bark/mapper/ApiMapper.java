86dcfbeedc59d51322eaa5153a683bb50b7309f8
jngwk <jngwk.ch@gmail.com>
2024-04-17 16:55:38 +0900

    게시판 컨트롤러 공지사항 GET 추가, API 매퍼 클래스 어노테이션 추가

--- Spring/mySpring/src/main/java/com/bark/mapper/ApiMapper.java
+++ Spring/mySpring/src/main/java/com/bark/mapper/ApiMapper.java
@@ -1,9 +1,11 @@
 package com.bark.mapper;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.bark.domain.Shelter;
 
+@Mapper
 public interface ApiMapper {
 	@Insert("insert into shelter (shelterName, shelterAddr, lat, lng) values(#{shelterName},#{shelterAddr},#{lat},#{lng})")
 	public void putShelter(Shelter shelter);
