100eeeaf4ecd6e65992505cc3c80b105451e9bbc
jngwk <jngwk.ch@gmail.com>
2024-04-18 16:07:06 +0900

    bark renewed 내정보수정 화면, js 수정

--- bark-renewed/js/update-user copy.js
+++ bark-renewed/js/update-user copy.js
@@ -6,31 +6,30 @@ function openUpdatePop() {
 function closeUpdatePop() {
   document.getElementById("update-popup-layer").style.display = "none";
 }
-window.onload = function () {
-  const hypenTel = (target) => {
-    target.value = target.value
-      .replace(/[^0-9]/g, "")
-      .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
-  };
 
-  const pwdBtn = document.querySelector(".pwd-btn");
-  pwdBtn.onclick = function () {
-    document.querySelector(".pwd-mainmenu").classList.toggle("expand");
-  };
-
-  // 리셋버튼
-  let inputText = document.querySelectorAll(".input-text");
-  let resetBtn = document.querySelectorAll(".reset-btn");
-  inputText.forEach((input) => {
-    input.addEventListener("keyup", function () {
-      if (input.value == "") {
-        input.nextElementSibling.style.visibility = "hidden";
-      } else if (input.value != "") {
-        input.nextElementSibling.style.visibility = "visible";
-      }
-      input.nextElementSibling.onclick = function () {
-        this.style.visibility = "hidden";
-      };
-    });
-  });
+const hypenTel = (target) => {
+  target.value = target.value
+    .replace(/[^0-9]/g, "")
+    .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
 };
+
+const pwdBtn = document.querySelector(".pwd-btn");
+pwdBtn.onclick = function () {
+  document.querySelector(".pwd-mainmenu").classList.toggle("expand");
+};
+
+// 리셋버튼
+let inputText = document.querySelectorAll(".input-text");
+let resetBtn = document.querySelectorAll(".reset-btn");
+inputText.forEach((input) => {
+  input.addEventListener("keyup", function () {
+    if (input.value == "") {
+      input.nextElementSibling.style.visibility = "hidden";
+    } else if (input.value != "") {
+      input.nextElementSibling.style.visibility = "visible";
+    }
+    input.nextElementSibling.onclick = function () {
+      this.style.visibility = "hidden";
+    };
+  });
+});
