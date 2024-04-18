139eea5d6be8d07cd528318096f037f9d2e349cf
jngwk <jngwk.ch@gmail.com>
2024-04-18 22:35:17 +0900

    spring security test

--- Spring/mySpring/src/main/java/com/bark/mapper/UserMapper.java
+++ Spring/mySpring/src/main/java/com/bark/mapper/UserMapper.java
@@ -9,11 +9,15 @@ import com.bark.domain.User;
 public interface UserMapper {
 //	@Select("select * from user")
 	public List<User> getUserList();
-	public List<User> getAdminList();
+	
+
 	public User getUser(String id);
 	public int insert(User user);
 	public int update(User user);
 	public int delete(User user);
-	@Select("select User from user where email = #{email}")
+//	@Select("select User from user where email = #{email}")
     public User findUser(String email);
+	public List<User> getAdminList();
+
+	
 }
