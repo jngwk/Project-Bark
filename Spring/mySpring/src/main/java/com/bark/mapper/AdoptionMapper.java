96594b84d16d9bae7ceb21e8311981fbc94ec5eb
Z-hyeon <jihyeon2368@naver.com>
2024-04-22 15:16:08 +0900

    입양 내역 검색 기능 작업 중

--- Spring/mySpring/src/main/java/com/bark/mapper/AdoptionMapper.java
+++ Spring/mySpring/src/main/java/com/bark/mapper/AdoptionMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import com.bark.domain.Adoption;
 import com.bark.domain.Dog;
 
 @Mapper
@@ -25,4 +26,25 @@ public interface AdoptionMapper {
 			+ "		on s.shelterno = d.shelter_shelterno"
 			+ "	where dogno = #{dogno};")
 	public Dog getDog(int dogno);
+	
+	@Select("select a.adoptionno no, a.user_id id, u.name, s.shelterName,d.name dogName,a.adopt_date date,a.state\r\n"
+		+ "	from adoption a join user u on a.user_id = u.id\r\n"
+		+ "    join dog d on d.adoption_adoptionno = a.adoptionno\r\n"
+		+ "    join shelter s on s.shelterno = d.shelter_shelterno;")
+	public List<Adoption> getAdoptionList();
+
+	@Select("select a.adoptionno no, a.user_id id, u.name, s.shelterName,d.name dogName,a.adopt_date date,a.state\r\n"
+			+ "	from adoption a join user u on a.user_id = u.id\r\n"
+			+ "    join dog d on d.adoption_adoptionno = a.adoptionno\r\n"
+			+ "    join shelter s on s.shelterno = d.shelter_shelterno\r\n"
+			+ "where ${param1} like concat('%',#{param2},'%')")
+	public List<Adoption> getSearchAdoption(String filter,String input);
+
+	
+	@Select("select a.adoptionno no, a.user_id id, u.name, s.shelterName,d.name dogName,a.adopt_date date,a.state\r\n"
+			+ "	from adoption a join user u on a.user_id = u.id\r\n"
+			+ "    join dog d on d.adoption_adoptionno = a.adoptionno\r\n"
+			+ "    join shelter s on s.shelterno = d.shelter_shelterno\r\n"
+			+ "where ${param1} like concat('%',#{param2},'%') and state=#{param3};")
+	public List<Adoption> getUserState(String filter,String input,int state);
 }
--- Spring/mySpring/src/main/java/com/bark/service/AdoptionService.java
+++ Spring/mySpring/src/main/java/com/bark/service/AdoptionService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bark.domain.Adoption;
 import com.bark.domain.Dog;
+import com.bark.domain.User;
 import com.bark.mapper.AdoptionMapper;
 
 import lombok.Setter;
@@ -25,5 +27,19 @@ public class AdoptionService {
 	public Dog getDog(int dogno) {
 		log.info("getDog..................");
 		return mapper.getDog(dogno);
+	}
+	
+	public List<Adoption> getAdoptionList(){
+		log.info("getAdoptionList..................");
+		return mapper.getAdoptionList();
+	}
+
+	public List<Adoption> getSearchAdoption(String filter, String input) {
+		return mapper.getSearchAdoption(filter,input);
+	}
+
+	public List<Adoption> getUserState(String filter, String input, int state) {
+		// TODO Auto-generated method stub
+		return mapper.getUserState(filter,input,state);
 	}
 }
